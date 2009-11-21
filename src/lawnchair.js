6a4e38eb9ad28544520be4e8a85c31d5e7c827cd
Brian LeRoux <brian@westcoastlogic.com>
2009-11-21 16:08:25 -0500

    uuid works: thank you @sintaxi for pointing that out!

--- src/lawnchair.js
+++ src/lawnchair.js
@@ -201,9 +201,33 @@ Lawnchair.prototype = {
 		return new Date().getTime(); 
 	},
 	
-	// Returns a unique identifier in the event one has not been supplied. 
-	uuid:function() {
-		return 'foobar'
+	// Returns a unique identifier
+	uuid:function(len, radix) {
+		// based on Robert Kieffer's randomUUID.js at http://www.broofa.com
+	  	var chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'.split(''); 
+		var uuid = [];
+	    radix = radix || chars.length;
+
+	    if (len) {
+	      	for (var i = 0; i < len; i++) uuid[i] = chars[0 | Math.random()*radix];
+	    } else {
+	      	// rfc4122, version 4 form
+	      	var r;
+
+	      	// rfc4122 requires these characters
+	      	uuid[8] = uuid[13] = uuid[18] = uuid[23] = '-';
+	      	uuid[14] = '4';
+
+	      	// Fill in random data.  At i==19 set the high bits of clock sequence as
+	      	// per rfc4122, sec. 4.1.5
+	      	for (var i = 0; i < 36; i++) {
+	        	if (!uuid[i]) {
+	          		r = 0 | Math.random()*16;
+	          		uuid[i] = chars[(i == 19) ? (r & 0x3) | 0x8 : r];
+	        	}
+	      	}
+		}
+	    return uuid.join('');
 	},
 	
 	// Serialize a JSON object as a string. 
