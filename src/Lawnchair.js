f56a2388843205944111d258bef31ca0504a839e
Ed Finkler <funkatron@gmail.com>
2010-02-26 15:38:10 -0500

    added air-async adaptor for asynchronous SQLite operations in AIR

--- src/Lawnchair.js
+++ src/Lawnchair.js
@@ -16,7 +16,8 @@ Lawnchair.prototype = {
 			'gears':window.GearsSQLiteAdaptor,
 			'dom':window.DOMStorageAdaptor,
 			'cookie':window.CookieAdaptor,
-			'air':window.AIRSQLiteAdaptor
+			'air':window.AIRSQLiteAdaptor,
+			'air-async':window.AIRSQLiteAsyncAdaptor
 		};
 	
 		this.adaptor = opts.adaptor ? new adaptors[opts.adaptor](opts) : new WebkitSQLiteAdaptor(opts);
