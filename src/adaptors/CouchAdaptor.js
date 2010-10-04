268e64657d412efa785e9c0f380513f1fd019301
brianleroux <brian@westcoastlogic.com>
2010-10-04 10:51:48 -0700

    fucking softabs

--- src/adaptors/CouchAdaptor.js
+++ src/adaptors/CouchAdaptor.js
@@ -5,15 +5,15 @@
  *
  */
 var CouchAdaptor = function(options) {
-	for (var i in LawnchairAdaptorHelpers) {
-		this[i] = LawnchairAdaptorHelpers[i];
-	}
-	this.init(options);
+    for (var i in LawnchairAdaptorHelpers) {
+        this[i] = LawnchairAdaptorHelpers[i];
+    }
+    this.init(options);
 };
 
 // FIXME - should the constructor accept a callback? init of db could take a while..
 CouchAdaptor.prototype = {
-	init: function(options) {
+    init: function(options) {
         // TODO - make name required in all lawnchairs
         if (options.name == undefined)
             throw("name required for the couch adaptor. try: new Lawnchair({name:'store', adaptor:'couch'})");
@@ -23,15 +23,15 @@ CouchAdaptor.prototype = {
         // only create a db if it hasn't been
         if (CouchDB.allDbs().indexOf(options.name) == -1) 
             this.db.createDb();
-	},
+    },
 
-	save: function(obj, callback) {
+    save: function(obj, callback) {
         if (obj.key) obj._id = obj.key;
         var result = this.db.save(obj)
           , cb = this.terseToVerboseCallback(callback);
         if (cb)
             cb(obj);
-	},
+    },
     
     get: function(keyOrObject, callback) {
         var key = typeof keyOrObject == 'object' ? keyOrObject.key : keyOrObject
@@ -39,7 +39,7 @@ CouchAdaptor.prototype = {
         cb(this.db.open(key));
     },
 
-	all: function(callback) {
+    all: function(callback) {
         var map = function(doc) {
             if (!doc.key) doc.key = doc._id;  
             emit(doc.key, doc);
@@ -53,9 +53,9 @@ CouchAdaptor.prototype = {
           }
         if (cb)   
             cb(result);
-	},
+    },
 
-	remove: function(keyOrObj, callback) {
+    remove: function(keyOrObj, callback) {
         var cb = this.terseToVerboseCallback(callback)
           , me = this;
         if (typeof keyOrObj == 'object') {
@@ -69,14 +69,14 @@ CouchAdaptor.prototype = {
                     cb();
             });
         }
-	},
+    },
 
-	nuke: function(cb) {
+    nuke: function(cb) {
         this.db.deleteDb();
         this.db.createDb();
         if (cb)
             cb();
-	}, 
+    }, 
     // FIXME - not sure this is the right way to do this; would be useful for conditional code
     // that wants to check for capabilties on adaptor. (Eg, store.adaptor.db.replicate())
     toString: function() {
