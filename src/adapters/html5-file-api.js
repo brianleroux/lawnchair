a17e79cb418fa10c193edeec595171d60671e0ed
Cameron Hunter <hello@cameronhunter.co.uk>
2012-08-09 22:24:52 +0100

    Adding inital API template for html5-file-api lawnchair adapter

---
+++ src/adapters/html5-file-api.js
@@ -0,0 +1,35 @@
+Lawnchair.adapter('html5-file-api', (function(){
+
+    return {
+        // boolean; true if the adapter is valid for the current environment
+        valid: function() {},
+
+        // constructor call and callback. 'name' is the most common option 
+        init: function(options, callback) {},
+
+        // returns all the keys in the store
+        keys: function(callback) {},
+
+        // save an object
+        save: function(obj, callback) {},
+
+        // batch save array of objs
+        batch: function(array, callback) {},
+
+        // retrieve obj (or array of objs) and apply callback to each
+        get: function(key /* or array */, callback) {},
+
+        // check if an obj exists in the collection
+        exists: function(key, callback) {},
+
+        // returns all the objs to the callback as an array
+        all: function(callback) {},
+
+        // remove a doc or collection of em
+        remove: function(key /* or array */, callback) {},
+
+        // destroy everything
+        nuke: function(callback) {}
+    };
+
+}()));
