2889b1ef5b0b436ca440b835add3cf4dd523ed66
Cameron Hunter <hello@cameronhunter.co.uk>
2012-08-10 02:34:26 +0100

    Created stateful FileSystem object to try to encapsulate the rather horrible HTML5 filesystem API

--- src/adapters/html5-file-api.js
+++ src/adapters/html5-file-api.js
@@ -1,21 +1,105 @@
 Lawnchair.adapter('html5-file-api', (function(global){
 
-    var error = function() { console.error( arguments ); };
+    var error = function() {};
 
     var TEMPORARY = global.TEMPORARY || webkitStorageInfo.TEMPORARY;
-    var PERMANENT = global.PERMANENT || webkitStorageInfo.PERMANENT;
+    var PERSISTENT = global.PERSISTENT || webkitStorageInfo.PERSISTENT;
     var requestFileSystem = global.requestFileSystem || global.webkitRequestFileSystem || global.moz_requestFileSystem;
+    var BlobBuilder = global.BlobBuilder || global.WebKitBlobBuilder;
 
-    var ls = function( reader, entries, callback ) {
-        entries = entries || [];
-        reader.readEntries(function( results ) {
-            if ( !results.length ) {
-                callback( entries.map(function(entry) { return entry.name; }) );
-            } else {
-                ls( reader, entries.concat( Array.prototype.slice.call( results ) ) );
-            }
-        }, error );
-    };
+    var FileSystem = (function() {
+
+        var init = function( options, callback ) {
+            var me = this;
+            requestFileSystem( (options.storage || TEMPORARY), (options.size || 1024*1024*1024), function( fs ) {
+                me.fs = fs;
+                me.root = fs.root;
+                me.cd( me.root );
+                callback( this );
+            }, error );
+        };
+
+        init.ls = function( reader, callback, entries ) {
+            var result = entries || [];
+            reader.readEntries(function( results ) {
+                if ( !results.length ) {
+                    if ( callback ) callback( result.map(function(entry) { return entry.name; }) );
+                } else {
+                    ls( reader, callback, result.concat( Array.prototype.slice.call( results ) ) );
+                }
+            }, error );
+        };
+
+        init.mkdir = function( root, name, callback ) {
+            root.getDirectory( name, {create:true}, function( directory ) {
+                if ( callback ) callback( directory );
+            }, error );
+        };
+
+        init.rm = function( root, name, callback ) {
+            root.getFile( name, {create:false}, function( file ) {
+                file.remove(function() {
+                    if ( callback ) callback();
+                }, error );
+            }, error );
+        };
+
+        init.rm_rf = function( root, name, callback ) {
+            root.getDirectory( name, {}, function( directory ) {
+                directory.removeRecursively(function() {
+                    if ( callback ) callback();
+                }, error );
+            }, error );
+        };
+
+        init.exists = function( root, name, callback ) {
+            root.getFile( name, {create:false}, function() {
+                if ( callback ) callback( true );
+            }, function() {
+                if ( callback ) callback( false );
+            });
+        };
+
+        init.fn = init.prototype;
+
+        init.fn.pwd = function( callback ) {
+            if ( callback ) callback( this.pwd );
+            return pwd;
+        };
+
+        init.fn.ls = function( callback, entries ) {
+            init.ls( this.pwd.createReader(), callback, entries );
+            return this;
+        };
+
+        init.fn.cd = function( name, callback ) {
+            this.pwd = name;
+            if ( callback ) callback();
+            return this;
+        };
+
+        init.fn.mkdir = function( name, callback ) {
+            init.mkdir( this.pwd, name, callback );
+            return this;
+        };
+
+        init.fn.rm = function( name, callback ) {
+            init.rm( this.pwd, name, callback );
+            return this;
+        };
+
+        init.fn.rm_rf = function( name, callback ) {
+            init.rm_rf( this.pwd, name, callback );
+            return this;
+        };
+
+        init.fn.exists = function( name, callback ) {
+            init.exists( this.pwd, name, callback );
+            return this;
+        };
+
+        return init;
+    }());
 
     // boolean; true if the adapter is valid for the current environment
     var valid = function() {
@@ -25,20 +109,19 @@ Lawnchair.adapter('html5-file-api', (function(global){
     // constructor call and callback. 'name' is the most common option
     var init = function( options, callback ) {
         var me = this;
-        requestFileSystem( PERMANENT, (options.size || 1024*1024), function( fs ) {
+        new FileSystem( options, function(fs) {
             me.fs = fs;
-            fs.root.getDirectory( options.name, {create:true}, function( directory ) {
-                me.root = directory;
+            me.fs.mkdir( options.name, function() {
+                ms.fs.cd( options.name );
                 if ( callback ) me.fn( me.name, callback ).call( me, me );
-            }, error );
-        }, error );
-        return this;
+            });
+        });
     };
 
     // returns all the keys in the store
     var keys = function( callback ) {
         var me = this;
-        ls( this.root.createReader(), [], function( entries ) {
+        this.fs.ls(function( entries ) {
             if ( callback ) me.fn( 'keys', callback ).call( me, entries );
         });
         return this;
@@ -46,10 +129,24 @@ Lawnchair.adapter('html5-file-api', (function(global){
 
     // save an object
     var save = function( obj, callback ) {
-        if ( callback ) this.lambda( callback ).call( this, obj );
+        var me = this;
+        this.root.getFile( obj.key, {create:true}, function( file ) {
+            file.createWriter(function( writer ) {
+
+                writer.onwriteend = function() {
+                    if ( callback ) me.lambda( callback ).call( me, obj );
+                };
+
+                var builder = new BlobBuilder();
+                builder.append( JSON.stringify( obj ) );
+                writer.write( builder.getBlob( 'text/plain' ) );
+
+            }, error );
+        }, error );
         return this;
     };
 
+    // UNTESTED
     // batch save array of objs
     var batch = function( array, callback ) {
         var saved = [];
@@ -57,25 +154,45 @@ Lawnchair.adapter('html5-file-api', (function(global){
         return this;
     };
 
+    // DOESN'T SEEM TO WORK - gets as far as the FileReader
     // retrieve obj (or array of objs) and apply callback to each
     var get = function( key /* or array */, callback ) {
+        var me = this;
         if ( this.isArray( key ) ) {
             var values = [];
             if ( callback ) this.lambda( callback ).call( this, values );
         } else {
+            this.root.getFile( key, {create:false}, function( file ) {
+                var reader = new FileReader();
+
+                console.log( reader );
+
+                reader.onload = function(e) {
+                    console.log( arguments );
+                    if ( callback ) me.lambda( callback ).call( me, e.target.result );
+                };
+
+                reader.onerror = function() {
+                    console.error( arguments );
+                };
+
+                reader.readAsText( file );
+            }, error );
             var value = undefined;
-            if ( callback ) this.lambda( callback ).call( this, value );
         }
         return this;
     };
 
     // check if an obj exists in the collection
     var exists = function( key, callback ) {
-        var exists = false;
-        this.lambda( callback ).call( this, exists );
+        var me = this;
+        this.fs.exists( key, function( exists ) {
+            me.lambda( callback ).call( me, exists );
+        });
         return this;
     };
 
+    // TODO: IMPLEMENT
     // returns all the objs to the callback as an array
     var all = function( callback ) {
         var values = [];
@@ -85,13 +202,21 @@ Lawnchair.adapter('html5-file-api', (function(global){
 
     // remove a doc or collection of em
     var remove = function( key /* or object */, callback ) {
-        if ( callback ) this.lambda( callback ).call( this );
+        var me = this;
+        this.fs.rm( (key.key ? key.key : key), function() {
+            if ( callback ) me.lambda( callback ).call( me );
+        });
         return this;
     };
 
     // destroy everything
     var nuke = function( callback ) {
-        if ( callback ) this.lambda( callback ).call( this );
+        var me = this;
+        this.fs.rm_rf( this.root.name, function() {
+            me.fs.mkdir( me.root.name, function() {
+                if ( callback ) me.lambda( callback ).call( me );
+            });
+        });
         return this;
     };
 
