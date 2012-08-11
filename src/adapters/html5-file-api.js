3f2f48f94ab7ede1155d4af8eb7645f7a8d9bc51
Cameron Hunter <hello@cameronhunter.co.uk>
2012-08-11 16:58:22 +0100

    think it works!

--- src/adapters/html5-file-api.js
+++ src/adapters/html5-file-api.js
@@ -9,17 +9,37 @@ Lawnchair.adapter('html5-file-api', (function(global){
 
     var FileSystem = (function() {
 
-        var init = function( options, callback ) {
-            var me = this;
-            requestFileSystem( (options.storage || TEMPORARY), (options.size || 1024*1024*1024), function( fs ) {
-                me.fs = fs;
-                me.root = fs.root;
-                me.cd( me.root );
-                callback( this );
+        var cat = function( root, name, callback ) {
+            root.getFile( name, {create:false}, function( entry ) {
+                entry.file(function( file ) {
+                    var reader = new FileReader();
+
+                    reader.onerror = error;
+
+                    reader.onload = function(e) {
+                        callback( e.target.result );
+                    };
+
+                    reader.readAsText( file );
+                }, error );
             }, error );
         };
 
-        init.ls = function( reader, callback, entries ) {
+        var touch = function( root, name, contents, callback ) {
+            root.getFile( name, {create:true}, function( file ) {
+                file.createWriter(function( writer ) {
+                    writer.onwriteend = function() {
+                        if ( callback ) callback();
+                    };
+
+                    var builder = new BlobBuilder();
+                    builder.append( contents );
+                    writer.write( builder.getBlob( 'text/plain' ) );
+                }, error );
+            }, error );
+        };
+
+        var ls = function( reader, callback, entries ) {
             var result = entries || [];
             reader.readEntries(function( results ) {
                 if ( !results.length ) {
@@ -30,13 +50,13 @@ Lawnchair.adapter('html5-file-api', (function(global){
             }, error );
         };
 
-        init.mkdir = function( root, name, callback ) {
+        var mkdir = function( root, name, callback ) {
             root.getDirectory( name, {create:true}, function( directory ) {
                 if ( callback ) callback( directory );
             }, error );
         };
 
-        init.rm = function( root, name, callback ) {
+        var rm = function( root, name, callback ) {
             root.getFile( name, {create:false}, function( file ) {
                 file.remove(function() {
                     if ( callback ) callback();
@@ -44,7 +64,7 @@ Lawnchair.adapter('html5-file-api', (function(global){
             }, error );
         };
 
-        init.rm_rf = function( root, name, callback ) {
+        var rm_rf = function( root, name, callback ) {
             root.getDirectory( name, {}, function( directory ) {
                 directory.removeRecursively(function() {
                     if ( callback ) callback();
@@ -52,7 +72,7 @@ Lawnchair.adapter('html5-file-api', (function(global){
             }, error );
         };
 
-        init.exists = function( root, name, callback ) {
+        var exists = function( root, name, callback ) {
             root.getFile( name, {create:false}, function() {
                 if ( callback ) callback( true );
             }, function() {
@@ -60,41 +80,59 @@ Lawnchair.adapter('html5-file-api', (function(global){
             });
         };
 
-        init.fn = init.prototype;
-
-        init.fn.pwd = function( callback ) {
-            if ( callback ) callback( this.pwd );
-            return pwd;
+        var init = function( options, callback ) {
+            var me = this;
+            requestFileSystem( (options.storage || TEMPORARY), (options.size || 1024*1024*1024), function( fs ) {
+                me.fs = fs;
+                me.root = fs.root;
+                me.pwd = fs.root;
+                callback( me );
+            }, error );
         };
 
-        init.fn.ls = function( callback, entries ) {
-            init.ls( this.pwd.createReader(), callback, entries );
+        init.fn = init.prototype;
+
+        init.fn.cd = function( name, callback ) {
+            var me = this;
+            this.pwd.getDirectory( name, {create:false}, function( directory ) {
+                me.pwd = directory;
+                if ( callback ) callback( me.pwd );
+            });
             return this;
         };
 
-        init.fn.cd = function( name, callback ) {
-            this.pwd = name;
-            if ( callback ) callback();
+        init.fn.touch = function( name, contents, callback ) {
+            touch( this.pwd, name, contents, callback );
+            return this;
+        };
+
+        init.fn.ls = function( callback, entries ) {
+            ls( this.pwd.createReader(), callback, entries );
+            return this;
+        };
+
+        init.fn.cat = function( name, callback ) {
+            cat( this.pwd, name, callback );
             return this;
         };
 
         init.fn.mkdir = function( name, callback ) {
-            init.mkdir( this.pwd, name, callback );
+            mkdir( this.pwd, name, callback );
             return this;
         };
 
         init.fn.rm = function( name, callback ) {
-            init.rm( this.pwd, name, callback );
+            rm( this.pwd, name, callback );
             return this;
         };
 
         init.fn.rm_rf = function( name, callback ) {
-            init.rm_rf( this.pwd, name, callback );
+            rm_rf( this.pwd, name, callback );
             return this;
         };
 
         init.fn.exists = function( name, callback ) {
-            init.exists( this.pwd, name, callback );
+            exists( this.pwd, name, callback );
             return this;
         };
 
@@ -109,10 +147,13 @@ Lawnchair.adapter('html5-file-api', (function(global){
     // constructor call and callback. 'name' is the most common option
     var init = function( options, callback ) {
         var me = this;
-        this.fs = new FileSystem( options );
-        this.fs.mkdir( options.name, function() {
-            me.fs.cd( options.name );
-            if ( callback ) me.fn( me.name, callback ).call( me, me );
+        new FileSystem( options, function( fs ) {
+            me.fs = fs;
+            me.fs.mkdir( options.name, function() {
+                me.fs.cd( options.name, function() {
+                    if ( callback ) me.fn( me.name, callback ).call( me, me );
+                });
+            });
         });
     };
 
@@ -128,55 +169,44 @@ Lawnchair.adapter('html5-file-api', (function(global){
     // save an object
     var save = function( obj, callback ) {
         var me = this;
-        this.root.getFile( obj.key, {create:true}, function( file ) {
-            file.createWriter(function( writer ) {
-
-                writer.onwriteend = function() {
-                    if ( callback ) me.lambda( callback ).call( me, obj );
-                };
-
-                var builder = new BlobBuilder();
-                builder.append( JSON.stringify( obj ) );
-                writer.write( builder.getBlob( 'text/plain' ) );
-
-            }, error );
-        }, error );
+        me.fs.touch( obj.key, JSON.stringify( obj ), function() {
+            if ( callback ) me.lambda( callback ).call( me, obj );
+        });
         return this;
     };
 
-    // UNTESTED
     // batch save array of objs
-    var batch = function( array, callback ) {
+    var batch = function( objs, callback ) {
+        var me = this;
         var saved = [];
-        if ( callback ) this.lambda( callback ).call( this, saved );
+        for ( var i = 0, il = objs.length; i < il; i++ ) {
+            me.save( objs[i], function( obj ) {
+                saved.push( obj );
+                if ( saved.length === il && callback ) {
+                    me.lambda( callback ).call( me, saved );
+                }
+            });
+        }
         return this;
     };
 
-    // DOESN'T SEEM TO WORK - gets as far as the FileReader
     // retrieve obj (or array of objs) and apply callback to each
     var get = function( key /* or array */, callback ) {
         var me = this;
         if ( this.isArray( key ) ) {
             var values = [];
-            if ( callback ) this.lambda( callback ).call( this, values );
+            for ( var i = 0, il = key.length; i < il; i++ ) {
+                me.get( key[i], function( result ) {
+                    values.push( result );
+                    if ( values.length === il && callback ) {
+                        me.lambda( callback ).call( me, values );
+                    }
+                });
+            }
         } else {
-            this.root.getFile( key, {create:false}, function( file ) {
-                var reader = new FileReader();
-
-                console.log( reader );
-
-                reader.onload = function(e) {
-                    console.log( arguments );
-                    if ( callback ) me.lambda( callback ).call( me, e.target.result );
-                };
-
-                reader.onerror = function() {
-                    console.error( arguments );
-                };
-
-                reader.readAsText( file );
-            }, error );
-            var value = undefined;
+            me.fs.cat( key, function( result ) {
+                if ( callback ) me.lambda( callback ).call( me, JSON.parse( result ) );
+            });
         }
         return this;
     };
@@ -185,16 +215,19 @@ Lawnchair.adapter('html5-file-api', (function(global){
     var exists = function( key, callback ) {
         var me = this;
         this.fs.exists( key, function( exists ) {
-            me.lambda( callback ).call( me, exists );
+            if ( callback ) me.lambda( callback ).call( me, exists );
         });
         return this;
     };
 
-    // TODO: IMPLEMENT
     // returns all the objs to the callback as an array
     var all = function( callback ) {
-        var values = [];
-        if ( callback ) this.fn( this.name, callback ).call( this, values );
+        var me = this;
+        this.keys(function( keys ) {
+            me.get( keys, function( values ) {
+                if ( callback ) me.fn( me.name, callback ).call( me, values );
+            });
+        });
         return this;
     };
 
@@ -210,10 +243,16 @@ Lawnchair.adapter('html5-file-api', (function(global){
     // destroy everything
     var nuke = function( callback ) {
         var me = this;
-        this.fs.rm_rf( this.root.name, function() {
-            me.fs.mkdir( me.root.name, function() {
-                if ( callback ) me.lambda( callback ).call( me );
-            });
+        var count = 0;
+        this.keys(function( keys ) {
+            for ( var i = 0, il = keys.length; i < il; i++ ) {
+                me.remove( keys[i], function() {
+                    count++;
+                    if ( count === il && callback ) {
+                        me.lambda( callback ).call( me );
+                    }
+                });
+            }
         });
         return this;
     };
